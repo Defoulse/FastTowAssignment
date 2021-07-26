3fe3252cca9e0be4d6f7f40aa0c1644385106641
Defoulse <aidarkhan3500@gmail.com>
2021-07-26 12:09:40 +0800

    Added SeedData with Default Admin User

---
+++ FastTowAssignment/Controllers/SeedData.cs
@@ -0,0 +1,42 @@
+﻿using FastTowAssignment.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastTowAssignment.Controllers
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new FastTowAssignmentContext(
+             serviceProvider.GetRequiredService<DbContextOptions<FastTowAssignmentContext>>()))
+            {
+                if (context.Users.Any())
+                {
+                    return;
+                }
+
+                context.Users.AddRange(
+                    new Areas.Identity.Data.FastTowAssignmentUser
+                    {
+                        Id = "d747aa55-cb04-4085-9a4b-d68651d5bd96",
+                        UserName = "Admin",
+                        Email = "admin@gmail.com",
+                        NormalizedEmail = "ADMIN@GMAIL.COM",
+                        EmailConfirmed = true,
+                        PasswordHash = "AQAAAAEAACcQAAAAEJJm39YelHtQm2PcIMc4jp8U5HLqN8j9pSHNZ7givlIKF49l38dzGh6SgPZSAMC2wg==",
+                        SecurityStamp = "FDSBIXZPEXL3RHECMWSHKMVRUW3JSIY2",
+                        ConcurrencyStamp = "2d700b01-b756-484d-8dbb-79050894ebda",
+                        LockoutEnabled = true
+                    }
+                );
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
--- FastTowAssignment/Program.cs
+++ FastTowAssignment/Program.cs
@@ -1,5 +1,9 @@
+using FastTowAssignment.Controllers;
+using FastTowAssignment.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +17,24 @@ namespace FastTowAssignment
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<FastTowAssignmentContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger =
+                    services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
