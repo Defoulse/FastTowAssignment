c9fa65a2d0849057680dc325f8815a32df841bcb
Defoulse <aidarkhan3500@gmail.com>
2021-07-26 16:27:12 +0800

    New Initial migration

--- FastTowAssignment/Migrations/20210726082539_initial.Designer.cs
+++ FastTowAssignment/Migrations/20210726082539_initial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FastTowAssignment.Migrations
 {
     [DbContext(typeof(FastTowAssignmentContext))]
-    [Migration("20210726081841_initial")]
+    [Migration("20210726082539_initial")]
     partial class initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
